7305e5701d4c63ccc8934b05225c6277496c24cb
FunkyFlier <mike@kaslingaircraft.com>
2016-05-27 07:29:47 -0500

    Add example code

--- inc/defines.h
+++ inc/defines.h
@@ -1,15 +1,16 @@
-/*
- * defines.h
- *
- *  Created on: May 27, 2016
- *      Author: work
- */
-
 #ifndef DEFINES_H_
 #define DEFINES_H_
 
+//defines for reset and clock control from the cube or datasheet
+#define RCC_OSCILLATORTYPE    RCC_OSCILLATORTYPE_HSE /*!< Used to select system oscillator type */
+#define RCC_PLLM              4                      /*!< Used for PLL M parameter */
+#define RCC_PLLN              84                    /*!< Used for PLL N parameter */
+#define RCC_PLLP              2                      /*!< Used for PLL P parameter */
+#define RCC_PLLQ              7                      /*!< Used for PLL Q parameter */
+#define RCC_PLLR              2                     /*!< Used for PLL R parameter, available on STM32F446xx */
 
 
-
+#define STM32F401_DISCOVERY
+#define STM32F4xx
 
 #endif /* DEFINES_H_ */
--- src/main.c
+++ src/main.c
@@ -7,14 +7,39 @@
   * @brief   Default main function.
   ******************************************************************************
 */
+#include "attributes.h"
+#include "stm32fxxx_hal.h"
+#include "defines.h"
+#include "tm_stm32_disco.h"
+#include "tm_stm32_delay.h"
 
+int main(void) {
+    /* Init system clock for maximum system speed */
+    TM_RCC_InitSystem();
 
-#include "stm32f4xx.h"
-#include "stm32f401_discovery.h"
-			
+    /* Init HAL layer */
+    HAL_Init();
 
-int main(void)
-{
+    /* Init leds */
+    TM_DISCO_LedInit();
 
-	for(;;);
+    /* Init button */
+    TM_DISCO_ButtonInit();
+
+    /* Init delay */
+    TM_DELAY_Init();
+
+    while (1) {
+        /* Turn off ALL leds */
+        TM_DISCO_LedToggle(LED_ALL);
+
+        /* If button pressed */
+        if (TM_DISCO_ButtonPressed()) {
+            /* Delayms */
+            Delayms(1000);
+        } else {
+            /* Delayus */
+            Delay(100000);
+        }
+    }
 }
