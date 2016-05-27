3d109be9c38f2176f271bf79371355452907ef32
FunkyFlier <mike@kaslingaircraft.com>
2016-05-27 08:05:37 -0500

    Switch to blink via delay example

--- src/main.c
+++ src/main.c
@@ -12,13 +12,6 @@
 #include "defines.h"
 #include "tm_stm32_disco.h"
 #include "tm_stm32_delay.h"
-/* Create pointers for 2 timers */
-TM_DELAY_Timer_t* SWTIM1;
-TM_DELAY_Timer_t* SWTIM2;
-
-/* References for function callbacks */
-void SWTIM1_Callback(TM_DELAY_Timer_t* SWTIM, void* UserParameters);
-void SWTIM2_Callback(TM_DELAY_Timer_t* SWTIM, void* UserParameters);
 
 int main(void) {
     /* Init system clock for maximum system speed */
@@ -30,34 +23,17 @@ int main(void) {
     /* Init leds */
     TM_DISCO_LedInit();
 
-    /* Init button */
-    TM_DISCO_ButtonInit();
-
     /* Init delay */
     TM_DELAY_Init();
 
-    /* Create software timers */
-
-    /* 100ms with autoreload enabled, and start after it is created */
-    SWTIM1 = TM_DELAY_TimerCreate(100, 1, 1, SWTIM1_Callback, NULL);
-
-    /* 150ms with autoreload enabled, and start after it is created */
-    SWTIM2 = TM_DELAY_TimerCreate(150, 1, 1, SWTIM2_Callback, NULL);
-
     while (1) {
-        /* Do nothing */
-        /* Everything is performed in stm32fxxx_it.c function where callback functions are called for software timers */
+        /* Each 500ms */
+        if (TM_DELAY_Time() >= 500) {
+            /* Reset time */
+            TM_DELAY_SetTime(0);
+
+            /* Toggle LED */
+            TM_DISCO_LedToggle(LED_ALL);
+        }
     }
-}
-
-/* Callback function for SWTIM1 */
-void SWTIM1_Callback(TM_DELAY_Timer_t* SWTIM, void* UserParameters) {
-    /* Toggle green led */
-    TM_DISCO_LedToggle(LED_GREEN);
-}
-
-/* Callback function for SWTIM2 */
-void SWTIM2_Callback(TM_DELAY_Timer_t* SWTIM, void* UserParameters) {
-    /* Toggle red led */
-    TM_DISCO_LedToggle(LED_RED);
 }
